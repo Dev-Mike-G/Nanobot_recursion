458705c55c5d56ff8f7108e2ee1cb5ab49e42ba8
Mike Garcia <83309014+Dev-Mike-G@users.noreply.github.com>
2021-05-13 13:27:56 -0500

    Create nanotech.py

---
+++ nanotech.py
@@ -0,0 +1,16 @@
+def nanotech(n):
+    '''
+    In Ray Kurzweil's book The Singularity Is Now, he details how nanotechnology will change the industry of manufacturing. 
+    Kurzweil specifies how nanotechnology will enable nanobots to create anything, including other nanobots.
+    This script creates a program that shows the amount of nanobots created given that each nanobot creates one or all nanobots help eachother build the next step in unison.
+    variable n decides how many nanobots start the process
+    '''
+    if n >= 3000000:
+        print('Simulation complete')
+    else:
+        print(n)
+        nanotech(n+n)
+
+for i in nanotech(4):
+    counter = 0
+    counter +=1
